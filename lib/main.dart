a8f532629361f524aa8ef05bee46e42509bb86db
Enes Kaçan <enes.kacan@agu.edu.tr>
2020-07-06 11:43:54 +0300

    bottom navigation bar created

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,51 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold()
+      home: Home()
     );
   }
 }
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+
+  final tabs = [
+    Center(child: Text("Feed"),),
+    Center(child: Text("Label"),),
+    Center(child: Text("Label"),)
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.star),
+              title: Text('Feed')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.star),
+              title: Text('Label')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.star),
+              title: Text('Label')),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
+
 
 
