4a55ff0784780e6f953486b4502508858806927b
Enes Kaçan <enes.kacan@agu.edu.tr>
2020-07-06 13:11:22 +0300

    feed card created

---
+++ lib/components/feed_card.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:sertifier/models/feed_model.dart';
+
+class FeedCard extends StatelessWidget {
+  final Feed feed;
+  const FeedCard(this.feed);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10, top: 25),
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.8,
+        height: MediaQuery.of(context).size.height * 0.4,
+        child: Column(
+        children: <Widget>[
+          Image(image: NetworkImage(feed.imgUrl),),
+          Text("blog.sertifier.com"),
+          Text(feed.title),
+          Text("${feed.readTimeMinutes} minutes"),
+        ],
+      ),),
+    );
+  }
+}
